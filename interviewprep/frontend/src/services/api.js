2887492c9dd0536fac34399cc58f6a6a58a45606
Derwin Bell <djbell261@gmail.com>
2026-04-16 11:32:03 -0400

    register feat added

--- interviewprep/frontend/src/services/api.js
+++ interviewprep/frontend/src/services/api.js
@@ -4,14 +4,17 @@ const api = axios.create({
   baseURL: "http://localhost:8099",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
