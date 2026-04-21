92701d31aa62c1448f6022be4ea63c9197357d6c
Derwin Bell <djbell261@gmail.com>
2026-04-21 00:02:09 -0400

    Allow deployed cors front

--- interviewprep/src/main/java/com/teamone/interviewprep/config/SecurityConfig.java
+++ interviewprep/src/main/java/com/teamone/interviewprep/config/SecurityConfig.java
@@ -67,7 +67,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://impartial-charm-production-497c.up.railway.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
